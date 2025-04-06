5664f6a630b6f143ebb14ce07707506449bdca53
ayeshazubair4002 <ayeshazubairafzal@gmail.com>
2025-04-06 21:11:40 +0500

    Add files via upload

---
+++ lab7 tasks/class.js
@@ -0,0 +1,10 @@
+let head=document.getElementsByClassName("h11");
+console.dir(head);
+console.log(head);
+
+
+let para=document.getElementsByTagName("p");
+console.dir(para);
+
+let f=document.getElementById("divE");
+console.log(f.innerHTML);
---
+++ lab7 tasks/task1.js
@@ -0,0 +1,9 @@
+
+
+let heading = document.getElementById("H1");
+
+let textNode = document.createTextNode(" - Ayesha Zubair, 52916");
+
+heading.append(textNode);
+
+console.log(heading);
